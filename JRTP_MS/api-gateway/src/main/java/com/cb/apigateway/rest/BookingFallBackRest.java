75010d5f96b07c5736ca746ca54eae1ec4ad97f6
Dhruv.k <dhruv.k@digitelenetworks.com>
2024-02-19 18:05:13 +0530

    implemented routes and filters for service down fallback

--- JRTP_MS/api-gateway/src/main/java/com/cb/apigateway/rest/BookingFallBackRest.java
+++ JRTP_MS/api-gateway/src/main/java/com/cb/apigateway/rest/BookingFallBackRest.java
@@ -1,2 +1,16 @@
-package com.cb.apigateway.rest;public class BookingFallBackRest {
+package com.cb.apigateway.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class BookingFallBackRest {
+
+    @GetMapping("/booking-fallback")
+    public Mono<String> bookingFallback() {
+        return Mono.just(
+                        "Booking Service is not working(maintanance). Please try after some time.");
+    }
+
 }
